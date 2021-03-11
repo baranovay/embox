3bc9fff6d481cb453eaa1807b3f4df95301613a6
Alex Kalmuk <alexkalmuk@gmail.com>
2021-03-11 17:54:09 +0300

    conf-prepare.sh

---
+++ project/pjsip/templates/x86-qemu-pjsip-tests/conf_prepare.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script uncomments (removes "//") the specified test in
+# conf/mods.conf and conf/system_start.inc.
+
+TEST=`echo $1 | sed 's/-/_/g'`
+
+case $TEST in
+	pjlib_test)
+		sed -i -E "s/\/\/(.*)$TEST/\1$TEST/g" conf/mods.conf
+		sed -i -E "s/\/\/(.*)\"$TEST\"(.*)/\1\"$TEST\"\2/g" conf/system_start.inc
+		;;
+	*)
+		echo "Availbale tests:"
+		echo "    pjlib-test"
+		exit 1
+		;;
+esac
